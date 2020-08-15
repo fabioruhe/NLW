ba6021bdf8eb8172c6773e28aae700c4d99d5ef1
Fábio Ruhe <fabioruhe@gmail.com>
2020-08-15 06:04:04 -0300

    fixed

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -1,5 +1,5 @@
-module.exports = async function(db,{proffyValue, classValue, classScheduleValues}){
-    //inserir dados na tabela de proffys
+module.exports = async function(db, {proffyValue, classValue, classScheduleValues}) {
+    // Inserir dados na tabela de teachers
     const insertedProffy = await db.run(`
         INSERT INTO proffys (
             name,
@@ -12,43 +12,45 @@ module.exports = async function(db,{proffyValue, classValue, classScheduleValues
             "${proffyValue.whatsapp}",
             "${proffyValue.bio}"
         );
+      
     `)
 
-            const proffy_id = insertedProffy.lastId
+    const proffy_id = insertedProffy.lastID
 
-    
-            //inserir dados na tabla de classes
-            const insertedClasses = await db.run(`
-        INSERT INTO classes (
-            subject,
-            cost,
-            proffy_id
-        ) VALUES (
-            "${classValue.subject}",
-            "${classValue.cost}",
-            "${classValue.proffy_id}"
-        );
+    // Inserir dados na tabela classes
+
+    const insertedClass = await db.run(`
+            INSERT INTO classes (
+                subject,
+                cost,
+                proffy_id
+            ) VALUES (
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
+            );
     `)
-    const class_id = insertedClasses.lastID
-            
-   //inserir dados na tabla de classes(é um array,usar estrutura de repetição)
-            const insertedAllclassScheduleValues = classScheduleValues.map((classScheduleValue) => {
-                return db.run(`
-                    INSERT INTO class_schedule (
-                        class_id,
-                        weekday,
-                        time_from,
-                        time_to
-                    ) VALUES (
-                        "${class_id},
-                        "${classScheduleValue.weekday}",
-                        "${classScheduleValue.time_from}",
-                        "${classScheduleValue.time_to}"
-                        
-                    );
-                    `)
 
-            })
+    const class_id = insertedClass.lastID 
 
-            await Promise.all(insertedAllclassScheduleValues)
+    // Inserir dados na tabela class_schendule
+    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+        return db.run(`
+            INSERT INTO class_schedule (
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            ) VALUES (
+                "${class_id}",
+                "${classScheduleValue.weekday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
+            );
+        `)
+    })
+
+    // Aqui vou executar todos os db.runs() das class_schedules
+    await Promise.all(insertedAllClassScheduleValues)
+
 }
--- src/database/test.js
+++ src/database/test.js
@@ -1,33 +1,33 @@
 const Database = require('./db')
 const createProffy = require('./createProffy')
+
+
 Database.then(async (db) => {
-    //inserir dados
-
+    // inserir dados
     proffyValue = {
-        name: "Diego Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "19999666364", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
     }
 
     classValue = {
-        subject: "Química", 
-        cost:"20",
-        //proffyID vem pelo DB
+        subject: "Quimica",
+        cost: "20,00",
+        // Proffy id vira pelo banco de dados
     }
 
     classScheduleValues = [
+        //class_id virá pelo banco de dados, após cadastrarmos a class
         {
-            //classId virá pelo DB
             weekday: 1,
             time_from: 720,
-            time_to: 1220
+            time_to:1220
         },
         {
             weekday: 0,
             time_from: 520,
-            time_to: 1220
+            time_to:1220
         }
     ]
 
