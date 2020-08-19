17319233d636eaf67fb4d1b8ed52ba2a3f4ceb0d
Fábio Ruhe <fabioruhe@gmail.com>
2020-08-19 08:32:43 -0300

    Done

--- src/database/db.js
+++ src/database/db.js
@@ -1,33 +1,28 @@
-const Database = require('sqlite-async')
+const Database = require('sqlite-async');
 
 function execute(db) {
-    //criar tabelas
-    //retorna as infos do DB para fora da função
-    return db.exec(`
-    CREATE TABLE IF NOT EXISTS proffys (
+  return db.exec(`
+      CREATE TABLE IF NOT EXISTS proffys (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
+        name TEXT, 
         avatar TEXT,
         whatsapp TEXT,
         bio TEXT
-    );
-
-    CREATE TABLE IF NOT EXISTS classes (
+      );
+      CREATE TABLE IF NOT EXISTS classes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        subject TEXT,
+        subject INTEGER,
         cost TEXT,
         proffy_id INTEGER
-    );
-
-    CREATE TABLE IF NOT EXISTS class_schedule(
+      );
+      CREATE TABLE IF NOT EXISTS class_schedule (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         class_id INTEGER,
         weekday INTEGER,
-        time_from,
-        time_to
-
-    );
-    `)
+        time_from INTEGER,
+        time_to INTEGER
+      );
+  `)
 }
-//export do DB para fora deste arquivo
-module.exports = Database.open(__dirname +'/database.sqlite').then(execute)
+
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute)    
